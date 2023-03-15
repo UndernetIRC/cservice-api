3f774b024b3945b6743af3dc0e2e552e6fa9c115
Stefan Wold <ratler@undernet.org>
2023-03-15 21:45:20 +0100

    test: add test coverage for db/types/password

--- db/types/password/password.go
+++ db/types/password/password.go
@@ -7,13 +7,16 @@ import (
 	pass "github.com/undernetirc/cservice-api/internal/auth/password"
 )
 
+// Password is a wrapper around the password hash
 type Password string
 
+// Validate validates the password
 func (p *Password) Validate(password string) error {
 	v := pass.GetValidatorFunc(string(*p))
 	return v(password)
 }
 
+// Set sets the password using the default hasher
 func (p *Password) Set(password string) error {
 	hash, err := pass.GenerateHash(pass.DefaultHasher, password)
 	if err != nil {
