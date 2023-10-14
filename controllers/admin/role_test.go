f044cef24b6c6df21fcb3c8d659c8b4fbc226c22
Stefan Wold <ratler@undernet.org>
2023-10-14 17:00:51 +0200

    test: test role removal endpoints

--- controllers/admin/role_test.go
+++ controllers/admin/role_test.go
@@ -99,6 +99,82 @@ func TestRoleController(t *testing.T) {
 		assert.Contains(t, errResp.Message, "users is a required field")
 	})
 
+	t.Run("RemoveUsersFromRole", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+		db.On("GetUsersByUsernames", mock.Anything, mock.Anything).
+			Return([]models.GetUsersByUsernamesRow{
+				{ID: 1, Username: "admin"},
+				{ID: 2, Username: "test"},
+			}, nil)
+		db.On("RemoveUsersFromRole", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id/users", c.RemoveUsersFromRole, echojwt.WithConfig(jwtConfig))
+
+		body := bytes.NewBufferString(`{"users": ["admin", "test"]}`)
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/1/users", body)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		resp := w.Result()
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("RemoveUsersFromRole_EmptyUserList", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id/users", c.RemoveUsersFromRole, echojwt.WithConfig(jwtConfig))
+
+		body := bytes.NewBufferString(``)
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/1/users", body)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		res := w.Result()
+		errResp := new(echo.HTTPError)
+		dec := json.NewDecoder(res.Body)
+		err := dec.Decode(&errResp)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+		assert.Contains(t, errResp.Message, "users is a required field")
+	})
+
+	t.Run("RemoveUsersFromRole_IncorrectRoleID", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id/users", c.RemoveUsersFromRole, echojwt.WithConfig(jwtConfig))
+
+		body := bytes.NewBufferString(``)
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/test/users", body)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		res := w.Result()
+		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
 	t.Run("CreateRole", func(t *testing.T) {
 		db := mocks.NewQuerier(t)
 		db.On("CreateRole", mock.Anything, mock.Anything).
@@ -125,6 +201,70 @@ func TestRoleController(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, http.StatusCreated, resp.StatusCode)
 		assert.Equal(t, int32(1), roleCeateResponse.ID)
+	})
+
+	t.Run("DeleteRole", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+		db.On("DeleteRole", mock.Anything, mock.Anything).
+			Return(nil)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id", c.DeleteRole, echojwt.WithConfig(jwtConfig))
+
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/1", nil)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		resp := w.Result()
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("DeleteRole_RoleNotFound", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+		db.On("DeleteRole", mock.Anything, mock.Anything).
+			Return(pgx.ErrNoRows)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id", c.DeleteRole, echojwt.WithConfig(jwtConfig))
+
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/1", nil)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		resp := w.Result()
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+
+	t.Run("DeleteRole_IncorrectID", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.DELETE("/:id", c.DeleteRole, echojwt.WithConfig(jwtConfig))
+
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodDelete, "/test", nil)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		e.ServeHTTP(w, r)
+		resp := w.Result()
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	})
 
 	t.Run("CreateRole_AlreadyExists", func(t *testing.T) {
@@ -228,4 +368,33 @@ func TestRoleController(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	})
 
+	t.Run("GetRoles", func(t *testing.T) {
+		db := mocks.NewQuerier(t)
+		db.On("ListRoles", mock.Anything).
+			Return([]models.Role{
+				{ID: 1, Name: "test", Description: "test"},
+				{ID: 2, Name: "test2", Description: "test2"},
+			}, nil)
+
+		checks.InitUser(context.Background(), db)
+		c := NewAdminRoleController(db)
+
+		e := echo.New()
+		e.Validator = helper.NewValidator()
+		e.GET("/", c.GetRoles, echojwt.WithConfig(jwtConfig))
+
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest(http.MethodGet, "/", nil)
+		r.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		r.Header.Set(echo.HeaderAuthorization, "Bearer "+tokens.AccessToken)
+
+		response := &RoleListResponse{}
+		e.ServeHTTP(w, r)
+		res := w.Result()
+		dec := json.NewDecoder(res.Body)
+		err := dec.Decode(&response)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+		assert.Equal(t, 2, len(response.Roles))
+	})
 }
